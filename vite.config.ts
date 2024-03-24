8504fbac656bf3095486909e7d38ab39bf585017
Dzmitry Tarasiuk <dimatarasiuk91@gmail.com>
2024-03-24 11:32:38 +0300

    refactor: change repo info

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [svgr(), react()],
+    base:"/ClevertecFrontendLab/TarasiukDima/",
     server: {
         host: true,
         port: 3000,
